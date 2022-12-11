025b934fa1ad41f3a36d0bafd8444d03073cd908
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2022-12-11 13:19:54 -0300

    Implementing jwt strategy and route access controlled by this guard strategy

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 
 @Controller()
 export class UsersController {
+  constructor(private readonly authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   public async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  public(@Request() req) {
     return req.user;
   }
 }
