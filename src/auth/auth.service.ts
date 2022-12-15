00ef5f0b4e7b06d78fe480091ee5797771b5f485
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2022-12-14 22:42:48 -0300

    created method to encrypt e decrypt texts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/users/user';
 import { Person } from 'src/articles/person';
+import { createCipheriv, createDecipheriv, randomBytes, scrypt } from 'crypto';
+import { promisify } from 'util';
 
 @Injectable()
 export class AuthService {
   constructor(
+    @Inject(forwardRef(() => UsersService))
     private usersService: UsersService,
     private jwtService: JwtService,
   ) {}
@@ -46,5 +49,34 @@ export class AuthService {
     return {
       access_token: this.jwtService.sign(payload),
     };
+  }
+
+  public async encrypt(textToEncrypt: string): Promise<Buffer> {
+    const iv = randomBytes(16);
+    const password = 'Password used to generate key';
+
+    const key = (await promisify(scrypt)(password, 'salt', 32)) as Buffer;
+    const cipher = createCipheriv('aes-256-ctr', key, iv);
+
+    const encryptedText = Buffer.concat([
+      cipher.update(textToEncrypt),
+      cipher.final(),
+    ]);
+
+    return encryptedText;
+  }
+
+  public async decrypt(encryptedText: Buffer): Promise<Buffer> {
+    const iv = randomBytes(16);
+
+    const key = (await promisify(scrypt)(encryptedText, 'salt', 32)) as Buffer;
+
+    const decipher = createDecipheriv('aes-256-ctr', key, iv);
+    const decryptedText = Buffer.concat([
+      decipher.update(encryptedText),
+      decipher.final(),
+    ]);
+
+    return decryptedText;
   }
 }
