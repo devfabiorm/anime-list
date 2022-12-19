45979de689996852a13b6b6300ef5330e0c7edb5
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2022-12-19 08:04:43 -0300

    Applying cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://mysite.test' },
+  });
 
   app.use(helmet());
 
